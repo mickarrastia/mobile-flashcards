d74093342c078f8f8076e1c4b7f26de211408556
mickarrastia <michael.arrastia@gmail.com>
2018-07-17 23:40:25 +0100

    Add basic quiz functionality

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,40 +1,96 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
+import TextButton from './TextButton'
+import {red, green, white, purple} from '../utils/colors'
 
 class Quiz extends Component {
 
   state = {
-    questionIndex: 0,
+    questionIdx: 0,
     correct: 0,
     incorrect: 0,
-    viewAnswer: false,
+    showAnswer: false,
     viewResult: false
+  }
+
+  handleToggleButton = () => {
+    this.setState((state) => ({
+      showAnswer: !state.showAnswer
+    }))
+  }
+
+  handleAnswer = (result) => {
+    this.setState((state) => ({
+      questionIdx : state.questionIdx + 1,
+      correct : result === 'correct' ? state.correct + 1 : state.correct,
+      incorrect : result === 'incorrect' ? state.incorrect + 1 : state.incorrect,
+      showAnswer: false
+    }))
   }
 
   render() {
     const { deck } = this.props
-    const totalQuestions = deck.questions.length
-    const { questionIndex } = this.state
+    const { questionIdx, showAnswer } = this.state
+    const showResult = questionIdx < deck.questions.length ? true : false
 
     return(
-      <View>
-        <Text>{questionIndex+1}/{totalQuestions}</Text>
-        {questionIndex < totalQuestions ?
-          <Text>{deck.questions[questionIndex].question}</Text> :
-          <View>
-            <Text>Correct: {this.state.correct}</Text>
-            <Text>Incorrect: {this.state.incorrect}</Text>
-          </View>
+      <View style={styles.quiz}>
+        {showResult
+          ?
+            <View style={styles.quiz}>
+              <Text>{showAnswer ? deck.questions[questionIdx].answer : deck.questions[questionIdx].question}</Text>
+              <TextButton
+                onPress={this.handleToggleButton}
+                style={{marginTop: 20, marginBottom: 50}}
+              >{showAnswer ? 'Question' : 'Answer'}</TextButton>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => this.handleAnswer('correct')}
+              >
+                <Text style={{color: white}}>Correct</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: red}]}
+                onPress={() => this.handleAnswer('incorrect')}
+              >
+                <Text style={{color: white}}>Incorrect</Text>
+              </TouchableOpacity>
+            </View>
+          :
+            <View style={styles.quiz}>
+              <Text>Correct: {this.state.correct}</Text>
+              <Text>Incorrect: {this.state.incorrect}</Text>
+            </View>
         }
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  quiz: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    width: 150,
+    height: 50,
+    backgroundColor: green,
+    borderRadius: 16,
+    padding: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
 function mapStateToProps (state, { navigation }) {
   const { deckId } = navigation.state.params
-
   return {
     deckId,
     deck: state[deckId]
---
+++ components/TextButton.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { red } from '../utils/colors'
+
+export default function TextButton ({ children, onPress, style = {} }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text style={[styles.text, style]}>{children}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    color: red,
+  }
+})
